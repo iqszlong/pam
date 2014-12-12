1ca8b035fec2d053f3df1363b3923b51dd76da27
hy <iqszlong@qq.com>
2014-12-12 17:49:52 +0800

    add file

---
+++ js/function.js
@@ -0,0 +1,152 @@
+$(function(){
+//返回顶部
+    $(window).scroll(function(){
+      if ($(window).scrollTop()>100){
+        $("#top").fadeIn();
+      }
+      else
+      {
+        $("#top").fadeOut();
+      }
+      });
+      //当点击跳转链接后，回到页面顶部位置
+      $( document ).on( "click", "#top", function() {
+        $('body,html').animate({scrollTop:0},500);
+         return false;
+      }); 
+
+      $( document ).on("click",'.navbar-form',function(){
+        $('.search-field').focus();
+      });
+
+      console.log(1);
+});
+
+(function() {
+  var app = angular.module('webSite', ['ngRoute']).
+    config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+          templateUrl: 'home.htm',
+          controller:function(){
+            $('#home').addClass('active').siblings('li').removeClass('active');
+          }
+        }).when('/notice', {
+          templateUrl: 'notice.htm',
+          controller:function(){
+            $('#notice').addClass('active').siblings('li').removeClass('active');
+          }
+        }).when('/notice/:id', {
+          templateUrl: 'notice_detail.htm',
+          controller:function($scope, $routeParams){
+            $('#notice').addClass('active').siblings('li').removeClass('active');
+             $scope.id = $routeParams.id;
+          }
+        }).when('/download', {
+          templateUrl: 'download.htm',
+          controller:function(){
+            $('#download').addClass('active').siblings('li').removeClass('active');
+          }
+        }).when('/log', {
+          templateUrl: 'log.htm',
+          controller:function(){
+            $('#log').addClass('active').siblings('li').removeClass('active');
+          }
+        }).when('/demo', {
+          controller:function(){
+            //$('#demo').addClass('active').siblings('li').removeClass('active');
+          }
+        }).when('/service', {
+          templateUrl: 'service.htm',
+          controller:function(){
+            $('#service').addClass('active').siblings('li').removeClass('active');
+          }
+        }).when('/apps', {
+          templateUrl: 'apps.htm',
+          controller:function(){
+            $('#apps').addClass('active').siblings('li').removeClass('active');
+          }
+        }).when('/appreciation', {
+          templateUrl: 'appreciation.htm',
+          controller:function(){
+            $('#appreciation').addClass('active').siblings('li').removeClass('active');
+          }
+        }).
+        otherwise({templateUrl: '404.htm'});
+  }]);
+  
+
+
+  app.directive('navBar',function(){
+      return{
+        restrict :'E',
+        templateUrl: "nav-bar.htm",
+        controller:function(){
+          this.navActive = 1;
+          this.selectNav = function(setNav) {
+              this.navActive = setNav;
+
+          };
+          this.isSelected = function(checkNav){
+            return this.navActive === checkNav;
+          };
+        },
+        controllerAs: 'navSelect'
+      };
+    });
+
+
+  app.directive('footerBar',function(){
+      return{
+        restrict :'E',
+        templateUrl: "footer-bar.htm"
+      };
+  });
+
+
+  app.directive('productFeature', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-feature.htm',
+      controller:['$http',function($http){
+          var features = this;
+          features.items = [];
+          $http.get('features.json').success(function(data){
+              features.items = data;
+          });
+      }],
+      controllerAs: 'features'
+    };
+  });
+
+  app.directive('productFeatureMedia', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-feature-media.htm',
+      controller:['$http',function($http){
+          var features = this;
+          features.items = [];
+          $http.get('features.json').success(function(data){
+              features.items = data;
+          });
+      }],
+      controllerAs: 'features'
+    };
+  });
+
+
+  app.controller('tabController', function(){
+    this.tab = 1;
+    this.selectTab = function(setTab) {
+        this.tab = setTab;
+
+    };
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
+
+
+
+
+})();
