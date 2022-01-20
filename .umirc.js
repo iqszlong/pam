7bd5f68a4659c79323d2f5152a11bf914251717b
hy <iqszlong@qq.com>
2022-01-20 22:20:29 +0800

    增加项目内容

---
+++ .umirc.js
@@ -0,0 +1,30 @@
+
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index.jsx' }
+      ]
+    }
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: false,
+      dynamicImport: false,
+      title: 'pam',
+      dll: false,
+      
+      routes: {
+        exclude: [
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
