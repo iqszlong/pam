2313c64081bef4465a6e6e3b7e444002b047f0e8
hy <iqszlong@qq.com>
2022-01-21 14:50:26 +0800

    增加插件和完善内容

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/lib/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 // import Style from './index.css';
+
+moment.locale('zh-cn');
 
 function BasicLayout(props) {
   return (
     <>
-      {props.children}
+      <ConfigProvider locale={zhCN}>
+        {props.children}
+      </ConfigProvider>
     </>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,63 +1,109 @@
 import { Component } from 'react';
-import { Image,Menu } from 'antd';
+import { Image, Menu, Button, Tooltip, message } from 'antd';
+import { CopyOutlined } from '@ant-design/icons';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Style from './index.css';
 import RatioBox from '../components/RatioBox';
+
+
+
+const host = '//localhost:3004';
 
 function PhotoItems(props) {
   const data = props.data || [];
   const listItems = data.map((item, index) => (
     <RatioBox className={Style.item} key={index} width="1" height="1">
       <Image
-      width='100%'
-      height='100%'
-      src={item.min}
-      alt={item.id}
-      preview={{
-        src: item.path,
-      }}
-    />
+        width="100%"
+        height="100%"
+        src={item.min}
+        alt={item.id}
+        preview={{
+          src: item.path,
+        }}
+      />
+      <div className={Style.ctrl}>
+        <CopyToClipboard onCopy={() => message.success('复制成功')} text={ item.path.includes('http') ? item.path : `http:${item.path}`}>
+          <Tooltip title="复制完整路径" placement="bottom">
+            <Button size="small">
+              <CopyOutlined />
+            </Button>
+          </Tooltip>
+        </CopyToClipboard>
+      </div>
       {/* <img className={Style.img} src={item.min} alt={item.id} /> */}
     </RatioBox>
   ));
   return <Image.PreviewGroup>{listItems}</Image.PreviewGroup>;
 }
 
-
-
 class index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      current:'RQ-star241',
-      menu:[],
+      current: 'RQ-star241',
+      data: {}, //所有数据
+      menu: [],
       list: [],
     };
   }
 
   componentDidMount() {
     //
-    const menuReq = new Request('http://localhost:3000/menu');
+    const menuReq = new Request(`${host}/menu`);
     fetch(menuReq)
       .then(res => res.json())
       .then(data => {
         //获取数据
-        console.info(data);
+        // console.info(data);
+        const frist = data[0];
+        const key = `${frist.dir}${frist.cag}`;
+        const os_name = frist.os_name;
         this.setState({
           menu: data,
+          current: key,
         });
+        this.getData(os_name, key);
       })
       .catch(error => {
         console.error(`${menuReq.url}请求错误`, error);
       });
-    //
-    const apiReq = new Request('http://localhost:3000/qiniu');
+  }
+
+  //导航点击事件
+  handleClick = e => {
+    // console.log('click ', e);
+    const key = e.key;
+    const menuItem = this.state.menu.filter(el => `${el.dir}${el.cag}` === key);
+    const space_name = menuItem[0]['os_name'];
+    // console.log(space_name);
+    if (this.state.current !== key) {
+      this.getData(space_name, key);
+      // this.setState({ current: e.key });
+    }
+  };
+
+  //获取数据
+  // space_name 空间名
+  // key 目录属性名 dir+cag
+  getData(space_name, key) {
+    if (this.state.data.hasOwnProperty(key)) {
+      this.setState({
+        list: this.state.data[`${key}`],
+        current: key,
+      });
+      return;
+    }
+    const apiReq = new Request(`${host}/${space_name}`);
     fetch(apiReq)
       .then(res => res.json())
       .then(data => {
         //获取数据
         // console.info(data);
         this.setState({
-          list: data['RQ-star241'],
+          data: data,
+          list: data[`${key}`],
+          current: key,
         });
       })
       .catch(error => {
@@ -65,25 +111,17 @@ class index extends Component {
       });
   }
 
-  handleClick = e => {
-    console.log('click ', e);
-    this.setState({ current: e.key });
-  };
-  
-
   render() {
     return (
       <>
         <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-          {
-            this.state.menu.map((item)=>
-              <Menu.Item key={`${item.dir}${item.cag}`}>
-              {item.dir}{item.cag}
+          {this.state.menu.map(item => (
+            <Menu.Item key={`${item.dir}${item.cag}`}>
+              <span>{item.title || ''}</span>
             </Menu.Item>
-            ) 
-           
-          }
+          ))}
         </Menu>
+
         <div className={Style.layout}>
           <PhotoItems data={this.state.list}></PhotoItems>
         </div>
