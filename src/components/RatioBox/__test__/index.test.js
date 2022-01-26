54d1e7ef8c39c8d1d3258f121fad3d386097c1ad
hy <iqszlong@qq.com>
2022-01-26 12:13:47 +0800

    自动化测试更新

--- src/components/RatioBox/__test__/index.test.js
+++ src/components/RatioBox/__test__/index.test.js
@@ -1,30 +1,52 @@
 import RatioBox from '..';
 import renderer from 'react-test-renderer';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 
-import { shallow ,render,mount} from 'enzyme';
+
+// import { shallow, render, mount } from 'enzyme';
+
+
+let container = null;
+beforeEach(() => {
+    // 创建一个 DOM 元素作为渲染目标
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // 退出时进行清理
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
 
 
 
-describe('html测试', () => {
-    it('标签测试', () => {
-        const wrapper = renderer.create(<RatioBox/>);
-        expect(wrapper.root.children.length).toBe(1);
-        const outerLayer = wrapper.root.children[0];
-        expect(outerLayer.type).toBe('div');
-    });
-    test('渲染测试', () => {
-        const wrapper = mount(<RatioBox></RatioBox>);
-        expect(wrapper).toMatchSnapshot();
+describe('React 渲染测试', () => {
+    test('渲染默认值', () => {
+        act(() => {
+            render(<RatioBox />, container);
+        });
+        const wrapper = container.querySelectorAll('div')[0]; //获取最外层div
+        const cssName = wrapper.attributes[0];  //获取第一个属性 className
+        const styleName = wrapper.attributes[1];//获取第二个属性style
+        
+        expect(cssName.value).toMatch("layout");//验证 className 的值
+        expect(styleName.value).toMatch("56.25%");//验证计算后的 style 的值
     });
 });
 
-describe('属性测试', () => {
-    test('className', () => {
-        const wrapper = shallow(<RatioBox className="test_css" width="10" height="5" />);
-        // console.log(wrapper.debug());
-        const outerLayer = wrapper.find('.layout');
-        // console.log(outerLayer.debug());
-        expect(outerLayer.prop('className')).toMatch('test_css');
-        expect(outerLayer.prop('style')).toEqual({paddingTop:"50%"});
-    });
-});
+
+
+// describe('属性测试', () => {
+//     test('className', () => {
+//         const wrapper = shallow(<RatioBox className="test_css" width="10" height="5" />);
+//         // console.log(wrapper.debug());
+//         const outerLayer = wrapper.find('.layout');
+//         // console.log(outerLayer.debug());
+//         expect(outerLayer.prop('className')).toMatch('test_css');
+//         expect(outerLayer.prop('style')).toEqual({ paddingTop: "50%" });
+//     });
+// });
+
--- src/pages/__tests__/index.test.js
+++ src/pages/__tests__/index.test.js
@@ -3,15 +3,6 @@ import renderer from 'react-test-renderer';
 
 
 
-describe('Page: index', () => {
-  it('html节点嵌套测试', () => {
-    const wrapper = renderer.create(<Index />);
-    expect(wrapper.root.children.length).toBe(2);
-    const outerLayer = wrapper.root.children[0];
-    expect(outerLayer.children.length).toBe(1);
-  });
-});
-
 
 describe('简单测试', () => {
   function getPost() {
